88aa66ef435d4a937fd769961bd03b051d4175ad
George Baker <gsbaker2000@icloud.com>
2019-01-03 14:56:51 +0000

    Managed to get the FoodDataAccessor to work so now I can build on this interface adding more functionality.

---
+++ src/sample/FoodDataAccessor.java
@@ -0,0 +1,55 @@
+package sample;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodDataAccessor {
+
+    private Connection connection;
+
+    public FoodDataAccessor(String driverClassName, String databaseUrl, String user, String password) throws SQLException, ClassNotFoundException {
+        Class.forName(driverClassName);
+        connection = DriverManager.getConnection(databaseUrl, user, password);
+    }
+
+    public void shutdown() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public List<Food> getFoodList() throws SQLException {
+        try (
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM food");
+                ){
+            List<Food> foodList = new ArrayList<>();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int calories = resultSet.getInt("calories");
+                Food food = new Food(id, name, calories);
+                foodList.add(food);
+            }
+            return foodList;
+        }
+    }
+
+    public List<String> getFoodNames() throws SQLException {
+        try (
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM food");
+        ){
+            List<String> foodNames = new ArrayList<>();
+
+            while (resultSet.next()) {
+                foodNames.add(resultSet.getString("name"));
+            }
+            return foodNames;
+        }
+    }
+
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,16 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
 
+    FoodDataAccessor foodDataAccessor;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        String driverClassName = "com.mysql.cj.jdbc.Driver";
+        String databaseUrl = "jdbc:mysql://localhost:3306/FoodDiaryDatabase?useSSL=false";
+        String user = "myuser";
+        String password = "password";
+
+        foodDataAccessor = new FoodDataAccessor(driverClassName, databaseUrl, user, password);
+        foodDataAccessor.getFoodList();
+        System.out.println(foodDataAccessor.getFoodNames());
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Food Diary");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
+    @Override
+    public void stop() throws Exception {
+        if (foodDataAccessor != null) {
+            foodDataAccessor.shutdown();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
